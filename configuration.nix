fca0ac164e446eb0342fee169f8bb9916162b95b
Alexandros Liarokapis <liarokapis.v@gmail.com>
2024-06-20 11:41:10 +0300

    add image preview

--- configuration.nix
+++ configuration.nix
@@ -103,6 +103,20 @@
         adminpassFile = config.age.secrets."adminpassFile.txt".path;
       };
 
+      settings.enabledPreviewProviders = [
+        "OC\\Preview\\BMP"
+        "OC\\Preview\\GIF"
+        "OC\\Preview\\JPEG"
+        "OC\\Preview\\Krita"
+        "OC\\Preview\\MarkDown"
+        "OC\\Preview\\MP3"
+        "OC\\Preview\\OpenDocument"
+        "OC\\Preview\\PNG"
+        "OC\\Preview\\TXT"
+        "OC\\Preview\\XBitmap"
+        "OC\\Preview\\HEIC"
+      ];
+
       settings = {
         overwriteprotocol = "https";
       };
